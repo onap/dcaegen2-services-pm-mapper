8b7fe8ffe49cba7fce52838e51bb46144c06bae2
JoeOLeary <joseph.o.leary@est.tech>
2019-02-19 09:28:04 +0000

    Add XML validation
    Issue-ID: DCAEGEN2-1217
    Change-Id: Ic76885ab1c8e020a2bded05a264092684a747461
    Signed-off-by: JoeOLeary <joseph.o.leary@est.tech>

--- src/test/java/utils/EventUtils.java
+++ src/test/java/utils/EventUtils.java
@@ -31,13 +31,11 @@ import java.nio.file.Files;
 import java.nio.file.Path;
 import java.util.HashMap;
 import java.util.List;
-import java.util.Map;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
 import org.onap.dcaegen2.services.pmmapper.model.Event;
 import org.onap.dcaegen2.services.pmmapper.model.EventMetadata;
-import org.slf4j.MDC;
 
 public class EventUtils {
 
