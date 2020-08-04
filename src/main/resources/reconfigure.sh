014b937f2fc9687a5d7f2d1ee59f22ddc55a3343
JoeOLeary <joseph.o.leary@est.tech>
2020-08-04 12:58:01 +0100

    Update Config Acquisation
    Issue-ID: DCAEGEN2-2341
    Change-Id: I3fc431d563e668cbc2bd4a1dd03e30ca908fed0b
    Signed-off-by: JoeOLeary <joseph.o.leary@est.tech>

--- src/main/resources/reconfigure.sh
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env sh
-while true
-do
-    sleep 60
-    echo $(curl -sI -X GET https://localhost:8443/reconfigure -k | head -n1) >> /var/log/ONAP/dcaegen2/services/pm-mapper/reconfigure.log
-done
