5df7c966b5141e090dc014fad435687a296a9e87
Kai <lukai@chinamobile.com>
2020-08-24 10:49:43 +0800

    E2E Network Slicing: KPI Monitoring - PM Mapper enhancements
    Issue-ID: DCAEGEN2-2339
    Signed-off-by: Kai Lu <lukai@chinamobile.com>
    Change-Id: I1ee3cb7a573664a52005d43058a5490493ea4a23

---
+++ src/main/java/org/onap/dcaegen2/services/pmmapper/kpi/datamodule/MeasInfo.java
@@ -0,0 +1,53 @@
+/*
+ * ================================================================================
+ * Copyright (c) 2020 China Mobile. All rights reserved.
+ * ================================================================================
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ * ============LICENSE_END=========================================================
+ *
+ */
+
+package org.onap.dcaegen2.services.pmmapper.kpi.datamodule;
+
+import java.util.List;
+
+import org.onap.dcaegen2.services.pmmapper.kpi.config.BaseModule;
+
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+
+@Data
+@EqualsAndHashCode(callSuper = true)
+public class MeasInfo extends BaseModule {
+
+	/**
+	 * serialVersionUID
+	 */
+	private static final long serialVersionUID = 1L;
+
+	/**
+	 * measInfoId
+	 */
+	private MeasInfoId measInfoId;
+	
+	/**
+	 * measTypes
+	 */
+	private MeasTypes measTypes;
+	
+	/**
+	 * measValuesList
+	 */
+	private List<MeasValues> measValuesList;
+	
+}
