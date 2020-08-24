04fbddc0c65e52956593d2f80d0f7b879158d340
Kai <lukai@chinamobile.com>
2020-08-24 19:16:50 +0800

    E2E Network Slicing: KPI Monitoring - PM Mapper enhancements
    Issue-ID: DCAEGEN2-2339
    Signed-off-by: Kai Lu <lukai@chinamobile.com>
    Change-Id: I40fa8e8c6733fc77d0a64e37ee4018d0f55394da

---
+++ src/main/java/org/onap/dcaegen2/services/pmmapper/kpi/computation/KpiPublisher.java
@@ -0,0 +1,75 @@
+/*-
+ * ============LICENSE_START=======================================================
+ *  Copyright (C) 2020 China Mobile.
+ * ================================================================================
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ * ============LICENSE_END=========================================================
+ */
+
+package org.onap.dcaegen2.services.pmmapper.kpi.computation;
+
+import java.util.List;
+
+import org.onap.dcaegen2.services.pmmapper.messagerouter.VESPublisher;
+import org.onap.dcaegen2.services.pmmapper.model.Event;
+import org.onap.logging.ref.slf4j.ONAPLogAdapter;
+import org.slf4j.LoggerFactory;
+
+import reactor.core.publisher.Flux;
+
+/**
+ * KPI computation and published.
+ *
+ * @author Kai Lu
+ */
+public class KpiPublisher {
+
+    private static final ONAPLogAdapter logger = new ONAPLogAdapter(LoggerFactory.getLogger(KpiPublisher.class));
+    private VESPublisher vESPublisher;
+
+    public KpiPublisher(VESPublisher vESPublisher) {
+        this.vESPublisher = vESPublisher;
+    }
+
+    /**
+     * do KPI computation.
+     *
+     * @param events    events
+     * @return Kpi event list
+     *
+     */
+    public Flux<List<Event>> kpiComputation(List<Event> events) {
+        logger.unwrap().info("Publishing KPI VES events to messagerouter.");
+        try {
+            events.forEach(e -> this.checkAndDoComputation(e));
+            logger.unwrap().info("KPI computation done successfully");
+        } catch (Exception e) {
+            logger.unwrap().error("KPI computation done failed.", e);
+            return Flux.empty();
+        }
+        return Flux.just(events);
+    }
+
+    private void checkAndDoComputation(Event event) {
+        String ves = event.getVes();
+        List<String> vesList = KpiComputation.checkAndDoComputation(ves, vESPublisher.getConfig());
+        if (vesList == null || vesList.size() <= 0) {
+            logger.unwrap().info("NO kpi data generated of event", event);
+        } else {
+            vesList.forEach(v -> vESPublisher.publish(v));
+        }
+    }
+
+}
