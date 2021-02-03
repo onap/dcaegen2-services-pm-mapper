444e4d92fde0e2f4537fd671356ce578aab93e2a
Tomasz Wrobel <tomasz.wrobel@nokia.com>
2021-02-03 09:32:09 +0100

    Add configuration number of threads in files processing.
    Issue-ID: DCAEGEN2-2600
    Signed-off-by: Tomasz Wrobel <tomasz.wrobel@nokia.com>
    Change-Id: I04feb27698ef21196ab218de4bdd97be1fc85284

--- src/test/java/org/onap/dcaegen2/services/pmmapper/config/FilesProcessingConfigTest.java
+++ src/test/java/org/onap/dcaegen2/services/pmmapper/config/FilesProcessingConfigTest.java
@@ -32,7 +32,15 @@ public class FilesProcessingConfigTest {
 
     private static final String ENV_LIMIT_RATE = "PROCESSING_LIMIT_RATE";
 
-    private EnvironmentReader mockEnvironmentReader = mock(EnvironmentReader.class);
+    private static final String ENV_THREADS_MULTIPLIER = "THREADS_MULTIPLIER";
+    private static final String ENV_PROCESSING_THREADS_COUNT = "PROCESSING_THREADS_COUNT";
+    private static final String THREADS_4 = "4";
+    private static final String MULTIPLIER_3 = "3";
+
+    private static final int EXPECTED_4 = 4;
+    private static final int EXPECTED_12 = 12;
+
+    private final EnvironmentReader mockEnvironmentReader = mock(EnvironmentReader.class);
     private FilesProcessingConfig filesProcessingConfig;
 
     @Test
@@ -62,5 +70,72 @@ public class FilesProcessingConfigTest {
         int limitRate = filesProcessingConfig.getLimitRate();
 
         assertEquals(1, limitRate);
+    }
+
+    @Test
+    public void shouldReturnCorrectThreadsCount_whenVariableIsSet() throws EnvironmentConfigException {
+        when(mockEnvironmentReader.getVariable(ENV_PROCESSING_THREADS_COUNT)).thenReturn(THREADS_4);
+        when(mockEnvironmentReader.getVariable(ENV_THREADS_MULTIPLIER)).thenReturn(MULTIPLIER_3);
+
+        filesProcessingConfig = new FilesProcessingConfig(mockEnvironmentReader);
+        int threadsCount = filesProcessingConfig.getThreadsCount();
+
+        assertEquals(EXPECTED_12, threadsCount);
+    }
+
+    @Test
+    public void shouldReturnCorrectThreadsCount_whenVariableMultiplierIsNotSet() throws EnvironmentConfigException {
+
+        when(mockEnvironmentReader.getVariable(ENV_PROCESSING_THREADS_COUNT)).thenReturn(THREADS_4);
+
+        filesProcessingConfig = new FilesProcessingConfig(mockEnvironmentReader);
+        int threadsCount = filesProcessingConfig.getThreadsCount();
+
+        assertEquals(EXPECTED_4, threadsCount);
+    }
+
+    @Test
+    public void shouldReturnCorrectThreadsCount_whenVariableThreadsIsNotSet() throws EnvironmentConfigException {
+        when(mockEnvironmentReader.getVariable(ENV_THREADS_MULTIPLIER)).thenReturn(MULTIPLIER_3);
+
+        filesProcessingConfig = new FilesProcessingConfig(mockEnvironmentReader);
+        int threadsCount = filesProcessingConfig.getThreadsCount();
+        int expected = Runtime.getRuntime().availableProcessors() * Integer.parseInt(MULTIPLIER_3);
+
+        assertEquals(expected, threadsCount);
+    }
+
+    @Test
+    public void shouldReturnCorrectThreadsCount_whenVariableThreadsAndMultiplierIsNotSet()
+        throws EnvironmentConfigException {
+        filesProcessingConfig = new FilesProcessingConfig(mockEnvironmentReader);
+        int threadsCount = filesProcessingConfig.getThreadsCount();
+        int expected = Runtime.getRuntime().availableProcessors();
+
+        assertEquals(expected, threadsCount);
+    }
+
+    @Test
+    public void shouldThrowEnvironmentConfigException_whenProcessingThreadsVariableHasWrongValue() {
+        when(mockEnvironmentReader.getVariable(ENV_PROCESSING_THREADS_COUNT)).thenReturn("not-an-int");
+        filesProcessingConfig = new FilesProcessingConfig(mockEnvironmentReader);
+        String expectedMessage = "PROCESSING_THREADS_COUNT environment variable has incorrect value.\n";
+        String causeMessage = "For input string: \"not-an-int\"";
+
+        Throwable exception = assertThrows(EnvironmentConfigException.class, () -> filesProcessingConfig.getThreadsCount());
+        assertEquals(expectedMessage, exception.getMessage());
+        assertEquals(causeMessage, exception.getCause().getMessage());
+    }
+
+    @Test
+    public void shouldThrowEnvironmentConfigException_whenThreadsMultiplierVariableHasWrongValue() {
+        when(mockEnvironmentReader.getVariable(ENV_THREADS_MULTIPLIER)).thenReturn("not-an-int");
+        filesProcessingConfig = new FilesProcessingConfig(mockEnvironmentReader);
+        String expectedMessage = "THREADS_MULTIPLIER environment variable has incorrect value.\n";
+        String causeMessage = "For input string: \"not-an-int\"";
+
+        Throwable exception = assertThrows(EnvironmentConfigException.class, () -> filesProcessingConfig.getThreadsCount());
+        assertEquals(expectedMessage, exception.getMessage());
+        assertEquals(causeMessage, exception.getCause().getMessage());
     }
 }
