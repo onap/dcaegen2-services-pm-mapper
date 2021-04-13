45baa014654dde9944ccd1dcfe78782d355ab6be
tkogut <tomasz.kogut@nokia.com>
2021-04-13 10:11:35 +0200

    [DCAE/PM-Mapper] Utilize DMaaP-Client in PM-Mapper
     - Bump mockserver libraries
     - Use dmaap-client for sending events to dmapp-mr
     - Extract Retry/Timeout configs to separate class
     - Extract logging utils to separate class
    
    Issue-ID: DCAEGEN2-2732
    Change-Id: I5d406e99fe1def078f102ff704df5312f5ae996b
    Signed-off-by: tkogut <tomasz.kogut@nokia.com>

--- src/main/java/org/onap/dcaegen2/services/pmmapper/messagerouter/VESPublisher.java
+++ src/main/java/org/onap/dcaegen2/services/pmmapper/messagerouter/VESPublisher.java
@@ -1,6 +1,7 @@
 /*-
  * ============LICENSE_START=======================================================
  *  Copyright (C) 2019 Nordix Foundation.
+ *  Copyright (C) 2021 Nokia.
  * ================================================================================
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -20,57 +21,74 @@
 
 package org.onap.dcaegen2.services.pmmapper.messagerouter;
 
-import java.nio.charset.StandardCharsets;
-import java.util.Base64;
-import java.util.List;
-
 import org.onap.dcaegen2.services.pmmapper.exceptions.MRPublisherException;
 import org.onap.dcaegen2.services.pmmapper.model.Event;
 import org.onap.dcaegen2.services.pmmapper.model.MapperConfig;
-import org.onap.dcaegen2.services.pmmapper.utils.RequestSender;
+import org.onap.dcaegen2.services.pmmapper.utils.DmaapRequestSender;
+import org.onap.dcaegen2.services.sdk.model.streams.AafCredentials;
+import org.onap.dcaegen2.services.sdk.model.streams.ImmutableAafCredentials;
+import org.onap.dcaegen2.services.sdk.rest.services.dmaap.client.model.DmaapResponse;
+import org.onap.dcaegen2.services.sdk.rest.services.dmaap.client.model.MessageRouterPublishResponse;
 import org.onap.logging.ref.slf4j.ONAPLogAdapter;
 import org.slf4j.LoggerFactory;
-
 import reactor.core.publisher.Flux;
+
+import java.util.List;
+import java.util.stream.Collectors;
 
 public class VESPublisher {
     private static final ONAPLogAdapter logger = new ONAPLogAdapter(LoggerFactory.getLogger(VESPublisher.class));
-    private RequestSender sender;
-    private MapperConfig config;
+    private final DmaapRequestSender sender;
+    private final MapperConfig config;
 
     public VESPublisher(MapperConfig config) {
-        this(config, new RequestSender());
+        this(config, new DmaapRequestSender());
     }
 
-    public VESPublisher(MapperConfig config, RequestSender sender) {
+    public VESPublisher(MapperConfig config, DmaapRequestSender sender) {
         this.sender = sender;
         this.config = config;
     }
 
     public Flux<Event> publish(List<Event> events) {
         logger.unwrap().info("Publishing VES events to messagerouter.");
-        Event event = events.get(0);
-        try {
-            events.forEach(e -> this.publish(e.getVes()));
-            logger.unwrap().info("Successfully published VES events to messagerouter.");
-        } catch (MRPublisherException e) {
-            logger.unwrap().error("Failed to publish VES event(s) to messagerouter.", e);
-            return Flux.empty();
-        }
-        return Flux.just(event);
+        Event first = events.get(0);
+        List<String> vesEvents = minifiedVesEvents(events);
+        return publishEvents(vesEvents)
+                .filter(DmaapResponse::failed)
+                .takeLast(1)
+                .flatMap(this::toFluxError)
+                .defaultIfEmpty(first)
+                .doOnComplete(() -> logger.unwrap().info("Successfully published VES events to messagerouter."))
+                .onErrorResume(this::resume);
     }
 
-    private void publish(String ves) {
-        try {
-            String topicUrl = config.getPublisherTopicUrl();
-            ves = ves.replaceAll("\n", "");
-            String userCredentials =  Base64.getEncoder()
-                .encodeToString((this.config.getPublisherUserName() + ":" +
-                    this.config.getPublisherPassword())
-                    .getBytes(StandardCharsets.UTF_8));
-            sender.send("POST", topicUrl, ves, userCredentials);
-        } catch (Exception e) {
-            throw new MRPublisherException(e.getMessage(), e);
-        }
+    private List<String> minifiedVesEvents(List<Event> events) {
+        return events.stream()
+                .map(Event::getVes)
+                .map(vesEvent -> vesEvent.replace("\n", ""))
+                .collect(Collectors.toList());
+    }
+
+    private Flux<MessageRouterPublishResponse> publishEvents(List<String> vesEvents) {
+        String topicUrl = config.getPublisherTopicUrl();
+        AafCredentials credentials = aafCredentials();
+        return sender.send(topicUrl, vesEvents, credentials);
+    }
+
+    private Flux<Event> toFluxError(MessageRouterPublishResponse response) {
+        return Flux.error(new MRPublisherException(response.failReason()));
+    }
+
+    private Flux<Event> resume(Throwable t) {
+        logger.unwrap().error("Failed to publish VES event(s) to messagerouter.", t);
+        return Flux.empty();
+    }
+
+    private AafCredentials aafCredentials() {
+        return ImmutableAafCredentials.builder()
+                .username(config.getPublisherUserName())
+                .password(config.getPublisherPassword())
+                .build();
     }
 }
---
+++ src/main/java/org/onap/dcaegen2/services/pmmapper/utils/SendersConfig.java
@@ -0,0 +1,34 @@
+/*-
+ * ============LICENSE_START=======================================================
+ *  Copyright (C) 2021 Nokia.
+ * ================================================================================
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ * ============LICENSE_END=========================================================
+ */
+
+package org.onap.dcaegen2.services.pmmapper.utils;
+
+import java.time.Duration;
+
+final class SendersConfig {
+
+    private SendersConfig() {
+        throw new IllegalStateException("SendersConfig class;shouldn't be constructed");
+    }
+
+    static final int MAX_RETRIES = 4;
+    static final Duration RETRY_INTERVAL = Duration.ofSeconds(1);
+    static final Duration READ_TIMEOUT = Duration.ofSeconds(5);
+}
