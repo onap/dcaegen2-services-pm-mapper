88825c6d3dd49326cb875c143e2f16166ec8c9d3
Lasse Kaihlavirta <l.kaihlavirt@partner.samsung.com>
2021-05-21 12:59:08 +0300

    Move PM-Mapper CSIT to project repo
    Issue-ID: DCAEGEN2-2746
    Signed-off-by: Lasse Kaihlavirta <l.kaihlavirt@partner.samsung.com>
    Change-Id: Ib96f8453fd7fecbaddaada1b11d3b8551bb7f8fa

---
+++ csit/plans/pmmapper/teardown.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+echo "Starting teardown script"
+TEST_PLANS_DIR=$WORKSPACE/plans/pmmapper
+mkdir -p $WORKSPACE/archives
+docker exec pmmapper /bin/sh -c "cat /var/log/ONAP/dcaegen2/services/pm-mapper/pm-mapper_output.log"
+docker-compose -f $TEST_PLANS_DIR/docker-compose.yml logs > $WORKSPACE/archives/pmmapper-docker-compose.log
+docker-compose -f $TEST_PLANS_DIR/docker-compose.yml down -v
