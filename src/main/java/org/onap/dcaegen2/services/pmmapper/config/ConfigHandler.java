e4b9f159bc42e75fdba2523b321bee15e643e3ea
Tomasz Wrobel <tomasz.wrobel@nokia.com>
2021-11-16 10:36:10 +0100

    Switch configuration provider to CBS Client - DCAE SDK
    Issue-ID: DCAEGEN2-2964
    Signed-off-by: Tomasz Wrobel <tomasz.wrobel@nokia.com>
    Change-Id: Iaaf170d209b77b9709cc202c131f9d3bdf1033ed

--- src/main/java/org/onap/dcaegen2/services/pmmapper/config/ConfigHandler.java
+++ src/main/java/org/onap/dcaegen2/services/pmmapper/config/ConfigHandler.java
@@ -21,17 +21,26 @@ package org.onap.dcaegen2.services.pmmapper.config;
 
 import com.google.gson.Gson;
 import com.google.gson.JsonObject;
+import io.vavr.control.Option;
 import org.onap.dcaegen2.services.pmmapper.exceptions.CBSServerError;
 import org.onap.dcaegen2.services.pmmapper.exceptions.EnvironmentConfigException;
 import org.onap.dcaegen2.services.pmmapper.exceptions.MapperConfigException;
+import org.onap.dcaegen2.services.pmmapper.mapping.Mapper;
 import org.onap.dcaegen2.services.pmmapper.utils.EnvironmentConfig;
 import org.onap.dcaegen2.services.pmmapper.model.MapperConfig;
 import org.onap.dcaegen2.services.pmmapper.utils.RequestSender;
 import org.onap.dcaegen2.services.pmmapper.utils.RequiredFieldDeserializer;
 
+import org.onap.dcaegen2.services.sdk.rest.services.cbs.client.api.CbsClientFactory;
+import org.onap.dcaegen2.services.sdk.rest.services.cbs.client.api.CbsRequests;
+import org.onap.dcaegen2.services.sdk.rest.services.cbs.client.model.CbsRequest;
+import org.onap.dcaegen2.services.sdk.rest.services.model.logging.RequestDiagnosticContext;
+import org.onap.dcaegen2.services.sdk.rest.services.cbs.client.model.CbsClientConfiguration;
+
 import org.onap.logging.ref.slf4j.ONAPLogAdapter;
 import org.slf4j.LoggerFactory;
 import com.google.gson.GsonBuilder;
+import reactor.core.publisher.Mono;
 
 /**
  * Handles the retrieval of the component spec-based PM-Mapper Configuration
@@ -39,9 +48,11 @@ import com.google.gson.GsonBuilder;
  */
 
 public class ConfigHandler {
+
     private static final ONAPLogAdapter logger = new ONAPLogAdapter(LoggerFactory.getLogger(ConfigHandler.class));
     private RequestSender sender;
     private EnvironmentConfig environmentConfig;
+    private MapperConfig mapperConfig;
 
     /**
      * Creates a ConfigHandler.
@@ -65,32 +76,57 @@ public class ConfigHandler {
      * @throws EnvironmentConfigException
      */
     public MapperConfig getMapperConfig() throws EnvironmentConfigException {
-        String mapperConfigJson = "";
-        String cbsSocketAddress = this.environmentConfig.getCBSHostName() + ":" + this.environmentConfig.getCBSPort();
-        String requestURL = "http://" + cbsSocketAddress + "/service_component/" + this.environmentConfig.getServiceName();
-        try {
-            logger.unwrap().info("Fetching pm-mapper configuration from Configbinding Service");
-            mapperConfigJson = sender.send(requestURL);
-        } catch (Exception exception) {
-            throw new CBSServerError("Error connecting to Configbinding Service: ", exception);
+
+        //Todo add single configuration request -> reconfiguration is handled in other PM-Mapper Class
+        final RequestDiagnosticContext diagnosticContext = RequestDiagnosticContext.create();
+        final CbsRequest request = CbsRequests.getConfiguration(diagnosticContext);
+
+        final CbsClientConfiguration config = CbsClientConfiguration.fromEnvironment();
+
+        final MapperConfig mapperConfig1;
+        CbsClientFactory.createCbsClient(config)
+            .flatMap(cbsClient -> cbsClient.get(request))
+            .subscribe(
+                this::handleConfigurationFromConsul,
+                this::handleError
+            );
+
+        //Todo If mapperConfig == null then return null and log that Configuration wasn't loaded from Consul
+        if (mapperConfig == null) {
+            logger.unwrap().error("Mapper Config is null");
         }
-        return convertMapperConfigToObject(mapperConfigJson);
+        return mapperConfig;
     }
 
-    private MapperConfig convertMapperConfigToObject(String mapperConfigJson) {
-        MapperConfig mapperConfig;
+    public MapperConfig getInitialConfiguration() {
+        final RequestDiagnosticContext diagnosticContext = RequestDiagnosticContext.create();
+        final CbsRequest request = CbsRequests.getConfiguration(diagnosticContext);
+
+        final CbsClientConfiguration config = CbsClientConfiguration.fromEnvironment();
+        JsonObject jsonObject = CbsClientFactory.createCbsClient(config)
+            .flatMap(cbsClient -> cbsClient.get(request))
+            .block();
+        handleConfigurationFromConsul(jsonObject);
+        return mapperConfig;
+    }
+
+    void handleConfigurationFromConsul(JsonObject jsonObject) {
+        logger.unwrap().info("Configuration update from Consul {}", jsonObject);
+
         try {
-            JsonObject config = new Gson().fromJson(mapperConfigJson, JsonObject.class);
             mapperConfig = new GsonBuilder()
-                    .registerTypeAdapter(MapperConfig.class, new RequiredFieldDeserializer<MapperConfig>())
-                    .create()
-                    .fromJson(config, MapperConfig.class);
+                .registerTypeAdapter(MapperConfig.class, new RequiredFieldDeserializer<MapperConfig>())
+                .create()
+                .fromJson(jsonObject, MapperConfig.class);
         } catch (Exception exception) {
-            String exceptionMessage = "Error parsing configuration, mapper config:\n" + mapperConfigJson;
+            String exceptionMessage = "Error parsing configuration, mapper config:\n" + jsonObject.getAsString();
             throw new MapperConfigException(exceptionMessage, exception);
         }
         logger.unwrap().info("Received pm-mapper configuration from ConfigBinding Service");
-        logger.unwrap().debug("Mapper configuration:\n{}", mapperConfig);
-        return mapperConfig;
+        logger.unwrap().info("Mapper configuration:\n{}", mapperConfig);
+    }
+
+    private void handleError(Throwable throwable) {
+        logger.unwrap().error("Unexpected error occurred during fetching configuration from Consul", throwable);
     }
 }
