534e5dd052e8261ae62568f6c669bcc5a7ec63a5
Tomasz Wrobel <tomasz.wrobel@nokia.com>
2022-04-06 14:21:50 +0200

    Disable TLS in DataRouter (CSIT)
    Issue-ID: DCAEGEN2-3037
    Signed-off-by: Tomasz Wrobel <tomasz.wrobel@nokia.com>
    Change-Id: I01dd54ac173ed2bf705071e2bca118a068680f0a

--- csit/plans/pmmapper/setup.sh
+++ csit/plans/pmmapper/setup.sh
@@ -72,22 +72,22 @@ done
 
 # Data Router Configuration.
 docker exec -i datarouter-prov sh -c \
-    "curl -k  -X PUT https://$DR_PROV_IP:8443/internal/api/NODES?val=dmaap-dr-node\|$GATEWAY_IP"
+    "curl -k  -X PUT http://$DR_PROV_IP:8080/internal/api/NODES?val=dmaap-dr-node\|$GATEWAY_IP"
 docker exec -i datarouter-prov sh -c \
-    "curl -k  -X PUT https://$DR_PROV_IP:8443/internal/api/PROV_AUTH_ADDRESSES?val=dmaap-dr-prov\|$GATEWAY_IP"
+    "curl -k  -X PUT http://$DR_PROV_IP:8080/internal/api/PROV_AUTH_ADDRESSES?val=dmaap-dr-prov\|$GATEWAY_IP"
 
 # Create PM Mapper feed and create PM Mapper subscriber on data router
 curl -v -X POST -H "Content-Type:application/vnd.dmaap-dr.feed" -H "X-DMAAP-DR-ON-BEHALF-OF:pmmapper" \
       --data-ascii @$TEST_PLANS_DIR/assets/createFeed.json \
-      --post301 --location-trusted -k https://${DR_PROV_IP}:8443
+      --post301 --location-trusted -k http://${DR_PROV_IP}:8080
 curl -v -X POST -H "Content-Type:application/vnd.dmaap-dr.subscription" -H "X-DMAAP-DR-ON-BEHALF-OF:pmmapper" \
       --data-ascii @$TEST_PLANS_DIR/assets/addSubscriber.json \
-      --post301 --location-trusted -k https://${DR_PROV_IP}:8443/subscribe/1
+      --post301 --location-trusted -k http://${DR_PROV_IP}:8080/subscribe/1
 
 docker cp pmmapper:/var/log/ONAP/dcaegen2/services/pm-mapper/pm-mapper_output.log /tmp/pmmapper.log
 sleep 10
 docker exec -it datarouter-prov sh -c "curl http://dmaap-dr-node:8080/internal/fetchProv"
-curl -k https://$DR_PROV_IP:8443/internal/prov
+curl -k http://$DR_PROV_IP:8080/internal/prov
 
 #Pass any variables required by Robot test suites in ROBOT_VARIABLES
 ROBOT_VARIABLES="-v DR_PROV_IP:${DR_PROV_IP} -v DMAAP_MR_IP:${DMAAP_MR_IP} -v CBS_IP:${CBS_IP} -v PMMAPPER_IP:${PMMAPPER_IP} -v DR_NODE_IP:${DR_NODE_IP}"
--- tools/development/subscribe-pm-mapper.sh
+++ tools/development/subscribe-pm-mapper.sh
@@ -18,4 +18,4 @@ source ./env/containers_ip
 
 # PM Mapper subscriber on data router
 
-curl -v -X POST -H "Content-Type:application/vnd.dmaap-dr.subscription" -H "X-DMAAP-DR-ON-BEHALF-OF:pmmapper" --data-ascii @./resources/addSubscriber.json --post301 --location-trusted -k https://localhost:8443/subscribe/1
+curl -v -X POST -H "Content-Type:application/vnd.dmaap-dr.subscription" -H "X-DMAAP-DR-ON-BEHALF-OF:pmmapper" --data-ascii @./resources/addSubscriber.json --post301 --location-trusted -k http://localhost:8080/subscribe/1
